95bc5d48f412f6a6a1b42e4ae3eaca47f0ecd080
rafa <rafabolsa1@gmail.com>
2024-10-18 18:54:30 +0200

    Ejercicio productos

--- servidor/practica11_busquedaItems/busqueda.php
+++ servidor/practica11_busquedaItems/busqueda.php
@@ -2,17 +2,17 @@
 include 'datos.php';
 if(isset($_GET["busqueda"])){
     $busqueda=strtolower($_GET["busqueda"]);
-    $busquedaSanitizada=htmlspecialchars($busqueda);
-    echo "Resultados de la busqueda: </br>";
+    //$busquedaSanitizada=htmlspecialchars($busqueda);
+    echo "<h2>Resultados de la busqueda: </h2>";
     echo "<ul>";
     foreach($productos as $id=>$producto){
         $se_encuentraNombre=strpos(strtolower($producto["nombre"]),$busqueda);
-        $se_encuentraDescripcion=strpos(strtolower($producto["categoria"]),$busqueda);
+        $se_encuentraDescripcion=strpos(strtolower($producto["categoría"]),$busqueda);
         if($se_encuentraNombre!==false || $se_encuentraDescripcion!==false){
-            echo "<li><a href='detalles.php?producto=$id'>Ver detalles {$producto['nombre']} - {$producto['categoria']}</a></li>";
+            echo "<li><a href='detalles.php?producto=$id'>Ver detalles {$producto['nombre']} - {$producto['categoría']}</a></li>";
         }
     }
-    echo "/ul";
+    echo "</ul>";
 
 }
 ?>
--- servidor/practica11_busquedaItems/detalles.php
+++ servidor/practica11_busquedaItems/detalles.php
@@ -4,11 +4,12 @@ include "datos.php";
 if(isset($_GET['producto'])){
     $idProducto= $_GET["producto"];
     if(isset($productos[$idProducto])){
+        $producto=$productos[$idProducto];
         echo "<h2>Detalles del Producto</h2>";
         echo "<p><strong>Nombre:</strong> {$producto['nombre']}</p>";
-        echo "<p><strong>Categoría:</strong> {$producto['categoria']}</p>";
+        echo "<p><strong>Categoría:</strong> {$producto['categoría']}</p>";
         echo "<p><strong>Precio:</strong> \${$producto['precio']}</p>";
-        echo "<p><strong>Descripción:</strong>{$producto['descripcion']}</p>";
+        echo "<p><strong>Descripción:</strong>{$producto['descripción']}</p>";
     }else{
         echo "<p>Producto no encontrado.</p>";
     }
