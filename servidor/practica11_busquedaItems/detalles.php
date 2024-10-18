9dab053e7022164fc970ef82acd3ac2f1d770714
rafa <rafabolsa1@gmail.com>
2024-10-18 18:41:30 +0200

    ok

--- servidor/practica11_busquedaItems/detalles.php
+++ servidor/practica11_busquedaItems/detalles.php
@@ -3,6 +3,14 @@ include "datos.php";
 
 if(isset($_GET['producto'])){
     $idProducto= $_GET["producto"];
-    foreach($productos as )
+    if(isset($productos[$idProducto])){
+        echo "<h2>Detalles del Producto</h2>";
+        echo "<p><strong>Nombre:</strong> {$producto['nombre']}</p>";
+        echo "<p><strong>Categoría:</strong> {$producto['categoria']}</p>";
+        echo "<p><strong>Precio:</strong> \${$producto['precio']}</p>";
+        echo "<p><strong>Descripción:</strong>{$producto['descripcion']}</p>";
+    }else{
+        echo "<p>Producto no encontrado.</p>";
+    }
 }
 ?>
