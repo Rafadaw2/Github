709eb6206d12cf6bed0a1c281e19a48b398f111f
rafa <rafabolsa1@gmail.com>
2024-10-18 18:15:12 +0200

    recreo

--- servidor/practica11_busquedaItems/busqueda.php
+++ servidor/practica11_busquedaItems/busqueda.php
@@ -4,9 +4,15 @@ if(isset($_GET["busqueda"])){
     $busqueda=strtolower($_GET["busqueda"]);
     $busquedaSanitizada=htmlspecialchars($busqueda);
     echo "Resultados de la busqueda: </br>";
-    foreach($productos as $producto){
-        $se_encuentra=strpos(strtolower($producto),$busqueda);
-        if($se_encuentra!==false)
+    echo "<ul>";
+    foreach($productos as $id=>$producto){
+        $se_encuentraNombre=strpos(strtolower($producto["nombre"]),$busqueda);
+        $se_encuentraDescripcion=strpos(strtolower($producto["categoria"]),$busqueda);
+        if($se_encuentraNombre!==false || $se_encuentraDescripcion!==false){
+            echo "<li><a href='detalles.php?producto=$id'>Ver detalles {$producto['nombre']} - {$producto['categoria']}</a></li>";
+        }
     }
+    echo "/ul";
+
 }
 ?>
---
+++ servidor/practica11_busquedaItems/detalles.php
@@ -0,0 +1,8 @@
+<?php
+include "datos.php";
+
+if(isset($_GET['producto'])){
+    $idProducto= $_GET["producto"];
+    foreach($productos as )
+}
+?>
