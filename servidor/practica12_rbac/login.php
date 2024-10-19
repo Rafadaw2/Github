f97ed20271a99cd109c6ed1315908e7c0bbe32f3
rafa <rafabolsa1@gmail.com>
2024-10-19 13:08:48 +0200

    19102024/13:09

--- servidor/practica12_rbac/login.php
+++ servidor/practica12_rbac/login.php
@@ -2,6 +2,28 @@
 session_start();
 include "roles.php";
 
-if()
+if(isset($_POST["username"],$_POST["password"])){
+    $usuario=$_POST["username"];
+    $contraseña=$_POST["password"];
+    foreach($usuarios as $clave=>$valor){
+        if($clave==$usuario && $valor["password"]==$contraseña){
+            $rol=$valor["rol"];
+            if(count($rol)==1){
+                if( $rol=="admin"){
+                    header("Location: admin.html");
+                }elseif($rol=="user"){
+                    header("Location: user.html");
+                }elseif($rol=="manager"){
+                    header("Location: manager.html");
+                }
+            }elseif(count($rol)>1){
+                
+            }
+        }
+    }
+    if(!isset($rol)){
+        echo "El usuario o contraseña son incorrectos.";
+    }
+}
 
 ?>
