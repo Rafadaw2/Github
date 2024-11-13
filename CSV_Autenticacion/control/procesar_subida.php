e93a844b3268977cf9604fb23ac3a8a852e76c5f
rafa <rafabolsa1@gmail.com>
2024-11-13 21:07:33 +0100

    13112024 21:07

--- CSV_Autenticacion/control/procesar_subida.php
+++ CSV_Autenticacion/control/procesar_subida.php
@@ -6,7 +6,7 @@ $username = "root";      // Nombre de usuario
 $password = "1234";      // Contraseña
 $dbname = "emergencia";    // Nombre de la base de datos
 
-$repositorio= new RepositorioMYSQL(
+$repositorio = new RepositorioMYSQL(
     $servername,
     $dbname,
     $username,
@@ -31,27 +31,67 @@ if (isset($_FILES['archivo_csv'])) {
 
     /*3. Mover el archivo a la carpeta del municipio, sobrescribiendo el
     archivo anterior*/
-    
+
     move_uploaded_file($rutaTemporal, $rutaFinal);
 
     //4.- Verificar la última fecha de actualización en la base de datos
+
     $lastUpdate = $repositorio->obtenerUltimaFechaReporte($municipio);
-    $newUpdate = filemtime($fileDestination); // Fecha de subida del archivo
+    $newUpdate = filemtime($rutaFinal); // Fecha de subida del archivo
 
     /* 5.- Buscar palabras clave en el campo necesidades de ayuda con la
     función strpos NOTA: ESTO SE DEBE HACER POR CADA NECESIDAD ESTABLECIDA*/
+    $personas_afectadas=$datos[1];
+
+    $comunicaciones_cortadas=$datos[2];
+    if($comunicaciones_cortadas=="SI"){
+        $comunicaciones_cortadas=1;
+    }else{
+        $comunicaciones_cortadas=0;
+    }
+    $necesidades_ayuda=$datos[3];
+    $agua=0;
     if (stripos($necesidades_ayuda, 'agua') !== false) {
         $agua = 1;
-        }
-        // Asignar a $otros si no se encuentra ninguna palab ra clave
-        if ($agua === 0 && $productos_limpieza === 0 &&
-        $viveres === 0 && $medicinas === 0) {
+    }
+
+    $productos_limpieza=0;
+    if (stripos($necesidades_ayuda, 'productos de limpieza') !== false) {
+        $productos_limpieza = 1;
+    }
+
+    $viveres=0;
+    if (stripos($necesidades_ayuda, 'viveres') !== false) {
+        $viveres = 1;
+    }
+
+    $medicinas=0;
+    if (stripos($necesidades_ayuda, 'medicinas') !== false) {
+        $medicinas = 1;
+    }
+    // Asignar a $otros si no se encuentra ninguna palab ra clave
+    
+    if (
+        $agua === 0 && $productos_limpieza === 0 &&
+        $viveres === 0 && $medicinas === 0
+    ) {
         $otros = $necesidades_ayuda;
-        }
-        //5.- Insertar o actualizar los datos en la base de datos
-        $repositorio->guardarEstadoMunicipio($municipio,
-        $personas_afectadas, $comunicaciones_cortadas, $agua,
-        $productos_limpieza, $viveres, $medicinas, $otros);
-        //6.- Cerrar el archivo
-        fclose($file);
+    }else{
+        $otros="";
+    }
+
+    //6.- Insertar o actualizar los datos en la base de datos
+    
+    $repositorio->guardarEstadoMunicipio(
+        $municipio,
+        $personas_afectadas,
+        $comunicaciones_cortadas,
+        $agua,
+        $productos_limpieza,
+        $viveres,
+        $medicinas,
+        $otros
+    );
+    //7.- Cerrar el archivo
+    fclose($file);
 }
